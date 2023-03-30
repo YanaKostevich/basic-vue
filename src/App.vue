478ac730795a314a66590d2187210b67813ad847
Jane Kostevich <yana.kostevich18@gmail.com>
2023-03-30 04:28:58 +0300

    basic-vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <button @:click="likes += 1">Likes</button>
+      <button @:click="dislikes += 1">DisLikes</button>
+    </div>
+
+    <div>
+      Likes <strong> {{ likes }}</strong>
+    </div>
+    <div>
+      DisLikes <strong> {{ dislikes }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+};
+</script>
+<style></style>
